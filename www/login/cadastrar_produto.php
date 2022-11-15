d453cf4f03a74cdb5cd0b65edce4878f141b7e41
Isadora Chagas <isagchagass@gmail.com>
2022-11-15 10:53:28 -0300

    Corrige o fluxo de cadastro de produto :)

--- www/login/cadastrar_produto.php
+++ www/login/cadastrar_produto.php
@@ -1,10 +1,10 @@
 <?php
     $nome = $_POST ['nomeproduto'];
     $preco = $_POST ['preco'];
+    $estoque = $_POST ['estoque'];
     $codigo = $_POST ['codigo'];
-    $strcon = mysqli_connect ("localhost","root","","login") or die ("Erro ao conectar com o banco");
-    $sql = "INSERT INTO produtos VALUES ('".$nome."' , '".$preco."', '".$codigo."');"; 
+    $strcon = mysqli_connect ("localhost","root","","papelaria") or die ("Erro ao conectar com o banco");
+    $sql = "INSERT INTO Produto(nome, preco, estoque, codigo) VALUES ('".$nome."' , '".$preco."', '".$estoque."', '".$codigo."');"; 
     mysqli_query($strcon, $sql) or die ('Erro ao tentar cadastrar registro'); 
     echo "Produto cadastrado com sucesso";
-
-    ?>
+ ?>
