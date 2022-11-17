1fcc86d5b24830b686912db73fa7ad308a19d1cc
Rebecca Machado <rebecca.moreira@hotmail.com>
2022-11-17 20:11:05 -0300

    Inicio estilo cadastro produtos

--- www/papelaria/index_produto.php
+++ www/papelaria/index_produto.php
@@ -4,25 +4,35 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <title>Cadastro de Produto</title>
 </head>
 <body>
 
+    <h1>Cadastrar novo Produto</h1>
 
     <form action="cadastro_produto.php" method="POST" >
-        <label>Nome produto</label>
-        <input type="text" name="nomeproduto"><br>
+        <div class="form-group">
+            <label>Nome produto</label>
+            <input type="text" name="nomeproduto"><br>
+        </div>
 
-        <label>Preço</label>
-        <input type="text" name="preco"><br>
+        <div class="form-group">
+            <label>Preço</label>
+            <input type="text" name="preco"><br>
+        </div>
 
-        <label>Estoque</label>
-        <input type="text" name="estoque"><br>
+        <div class="form-group">
+            <label>Estoque</label>
+            <input type="text" name="estoque"><br>
+        </div>
 
-        <label>Código:</label>
-        <input type="number" name="codigo"><br>
-
-        <button type="submit" name="enviar" >Enviar</button>
+        <div class="form-group">
+            <label>Código:</label>
+            <input type="number" name="codigo"><br>
+        </div>
+        
+        <button type="submit" name="enviar" class="btn btn-primary">Cadastrar</button>
     </form>
 
     <a href="index.php">Voltar</a>
