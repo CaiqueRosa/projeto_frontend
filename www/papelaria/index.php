9e94adc8fc95f5d4b4ea5dee73d6d4d7c21a39f7
Isadora Chagas <isagchagass@gmail.com>
2022-11-19 11:54:48 -0300

    Comeca a estilizacao da home page

--- www/papelaria/index.php
+++ www/papelaria/index.php
@@ -4,23 +4,57 @@ include('protect.php');
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Product</title>
-</head>
-<body>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <title>Papelaria</title>
+  </head>
 
+  <body>
+        <header>
+            <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #daf0f3;">
+                <a class="navbar-brand" href="#">Admin</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
-    <h4>Boas vindas, <?php echo $_SESSION['nome']; ?> </h4>
+                <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Link</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" href="#">Disabled</a>
+                        </li>
+                    </ul>
 
-    <div>
-    <a href="logout.php">Sair</a>
+                    <form class="form-inline my-2 my-lg-0">
+                        <input class="form-control mr-sm-2" type="search" placeholder="Search">
+                        <button class="btn btn-outline-info my-2 my-sm-0" type="submit">Search</button>
+                    </form>
+                </div>
+            </nav>
+        </header>
 
-    <a href="index_produto.php">Cadastrar Produto</a>
-    </div>
-</body>
+        <main role="main" class="container">
+            <h3 class="mt-5">Boas vindas, <?php echo $_SESSION['nome']; ?> !!</h3>
+            
+            <p class="lead">Pin a fixed-height footer to the bottom of the viewport in desktop browsers with this custom HTML and CSS. A fixed navbar has been added with <code>padding-top: 60px;</code> on the <code>body &gt; .container</code>.</p>
+            <p>Back to <a href="../sticky-footer/">the default sticky footer</a> minus the navbar.</p>
+                
+            <div style="padding-top: 3rem;">
+                <a style="text-decoration: underline" href="logout.php">Sair</a> |
+                <a style="text-decoration: underline" href="index_produto.php">Cadastrar Produto</a>
+            </div>
+        </main>
+
+    </body>
 </html>
